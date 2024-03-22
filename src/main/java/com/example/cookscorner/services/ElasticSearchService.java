801b6cf789b44468d7e6cf9389defedd837ee7b3
utkoleg <Mun_2015@bk.ru>
2024-03-22 23:02:55 +0500

    elastic search service ObjectMapperUtils

--- src/main/java/com/example/cookscorner/services/ElasticSearchService.java
+++ src/main/java/com/example/cookscorner/services/ElasticSearchService.java
@@ -1,37 +1,7 @@
 package com.example.cookscorner.services;
 
-import com.example.cookscorner.dto.ingredient.IngredientResponseDTO;
-import com.example.cookscorner.dto.recipe.RecipeResponseDTO;
 import com.example.cookscorner.entities.Recipe;
-import com.example.cookscorner.util.ObjectMapperUtils;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.ElasticsearchStatusException;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.unit.Fuzziness;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-
-
 public interface ElasticSearchService {
     public void createIndex();
     public void saveRecipe(Recipe recipe);
